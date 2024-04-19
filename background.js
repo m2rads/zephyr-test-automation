031b4258532652f56f5c2673db2ce42dec2ceba0
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-04-19 14:10:21 -0700

    add sidepanel

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+chrome.sidePanel
+.setPanelBehavior({ openPanelOnActionClick: true })
+.catch((error) => console.error(error));
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "log") {
       console.log(request.message);
